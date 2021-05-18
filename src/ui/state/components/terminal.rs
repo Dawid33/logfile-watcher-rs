accd2ba92907705bd9a98b7b885ebde734b68d16
Dawid33 <dawidsobczak@fastmail.com>
2021-05-18 09:38:51 +0100

    added files

---
+++ src/ui/state/components/terminal.rs
@@ -0,0 +1,25 @@
+use tui::widgets::Block;
+use tui::widgets::*;
+
+#[derive(Clone)]
+pub struct Terminal {
+    pub text: String,
+}
+
+impl Terminal {
+    pub fn new() -> Self {
+        Self {
+            text: String::from(""),
+        }
+    }
+
+    pub fn text(mut self, text: String) -> Self {
+        self.text = text;
+        self
+    }
+
+    pub fn view<'a>(&self) -> Paragraph<'a> {
+        let block = Block::default().borders(Borders::ALL);
+        Paragraph::new(self.text.clone()).block(block)
+    }
+}
